345810cf135ff3220334bb0d27b1597aa4734526
AZIZABADA10 <abadaaziz288@gmail.com>
2025-09-17 02:01:02 +0100

    l'ajoute de menu.h

---
+++ include/menu.h
@@ -0,0 +1,12 @@
+#ifndef MENU_H
+#define MENU_H
+
+int Menu();
+int MenuAjouter();
+int MenuAfficher();
+int MenuRecherche();
+int MenuModifier();
+int MenuStatistiques();
+int MenuSupp();
+
+#endif
